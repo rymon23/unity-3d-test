e2e6d2f11a5c115c0f92aade4a228dab87b10ee1
Ryan Monaghan <49878208+rymon23@users.noreply.github.com>
2022-10-10 20:05:41 -0400

    feat: Update faction system

--- Unity3dProject/Assets/Scripts/Faction.cs
+++ Unity3dProject/Assets/Scripts/Faction.cs
@@ -2,15 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
 public enum FactionRelationship
 {
     unset = -2,
     none = -1,
     ally = 0,
-    enemy = 1,
+    enemy = 1
 }
-
 
 [CreateAssetMenu(fileName = "New Faction", menuName = "Faction")]
 public class Faction : ScriptableObject
@@ -19,25 +17,41 @@ public class Faction : ScriptableObject
     public new string name;
 
     public HashSet<int> enemyRelations;
+
     public HashSet<int> allyRelations;
 
-    // PUBLIC FACING FOR THE EDITOR 
+    // PUBLIC FACING FOR THE EDITOR
     // TO DO: Find a better way
     public Faction[] allies;
+
     public Faction[] enemies;
 
-
-    public static FactionRelationship GetFactionRelationship(Faction factionA, Faction factionB)
+    public static FactionRelationship
+    GetFactionRelationship(Faction factionA, Faction factionB)
     {
-        Debug.Log("Faction - "  + factionA.name  +": enemies: " + factionA.enemyRelations.Count);
-        Debug.Log("Faction - "  + factionA.name  +": allies: " + factionA.allyRelations.Count);
+        if (factionA == null || factionB == null)
+            return FactionRelationship.none;
 
-        if (factionA.enemyRelations.Contains(factionB.GetInstanceID())) return FactionRelationship.enemy;
-        if (factionA.allyRelations.Contains(factionB.GetInstanceID())) return FactionRelationship.ally;
+        Debug
+            .Log("Faction - " +
+            factionA.name +
+            ": enemies: " +
+            factionA.enemyRelations.Count);
+        Debug
+            .Log("Faction - " +
+            factionA.name +
+            ": allies: " +
+            factionA.allyRelations.Count);
+
+        if (factionA.enemyRelations.Contains(factionB.GetInstanceID()))
+            return FactionRelationship.enemy;
+        if (factionA.allyRelations.Contains(factionB.GetInstanceID()))
+            return FactionRelationship.ally;
         return FactionRelationship.none;
     }
 
-    public static FactionRelationship GetMultiFactionRelationship(Faction[] factionsA, Faction[] factionsB)
+    public static FactionRelationship
+    GetMultiFactionRelationship(Faction[] factionsA, Faction[] factionsB)
     {
         FactionRelationship relation = FactionRelationship.none;
         for (int i = 0; i < factionsA.Length; i++)
@@ -55,7 +69,6 @@ public class Faction : ScriptableObject
         }
         return relation;
     }
-
 
     public void SetupRelations()
     {
--- Unity3dProject/Assets/Scripts/FactionsManager.cs
+++ Unity3dProject/Assets/Scripts/FactionsManager.cs
@@ -1,21 +1,28 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class FactionsManager : MonoBehaviour
 {
     public static FactionsManager current;
+
     public Faction[] factions;
 
     private void Awake()
     {
+        SetupFactionRelations();
+    }
+
+    private void SetupFactionRelations()
+    {
+        Debug.Log("Faction Manager - factions found: " + factions.Length);
+
         if (factions != null && factions.Length > 0)
         {
-
             for (int i = 0; i < factions.Length; i++)
             {
                 factions[i].SetupRelations();
             }
         }
+
+        Debug.Log("Faction Manager: Faction Relations Setup");
     }
 }
